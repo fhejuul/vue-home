dfbc4a65bba8b342f9b10fb00403bcdea29015d1
HeinF <fh@amujuul.dk>
2024-03-11 15:49:25 +0100

    Feat: Implemented view of courses with automatic pagination

--- src/views/InfoListView.vue
+++ src/views/InfoListView.vue
@@ -1,36 +1,86 @@
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, onBeforeUnmount} from 'vue'
 import InfoService from '../services/InfoService'
 import InfoLine from '../components/InfoLine.vue'
 
-
-
+let timer = null
+const paginationInterval = 100000 // Make dynamic or env variable
+const linesToShow = 10 // Make dynamic or env variable
 const courses = ref(null)
+const listData = ref([])
+const start = ref(0)
+const end = ref(0)
+const currentPage = ref(0)
+const lastPage = ref(0)
+
+function startTimer(){
+  timer = setInterval(() => {
+    turnPage()
+  }, paginationInterval)
+}
+
+function setPageNumber() {
+  currentPage.value = Math.ceil((end.value+1)/linesToShow)
+  lastPage.value = Math.ceil(courses.value.length/linesToShow)
+}
+
+function turnPage(){
+  if(courses && courses.value.length > linesToShow){
+    setPageNumber()
+    end.value += Math.min((courses.value.length-start.value), linesToShow)
+    try {
+      listData.value = courses.value.slice(start.value, end.value)
+    } catch (error) {
+      console.log(error)
+      start.value = 0
+      end.value = 0
+    }
+  
+    if(start.value < (end.value-(linesToShow-1))){
+      start.value += 10
+    } else {
+      start.value = 0
+      end.value = 0
+    }
+  }
+}
+
 
 onMounted(() => {
   InfoService.getLocation()
     .then((response) => {
       courses.value = response.data
-      console.log(courses.value)
+      start.value = 0
+      turnPage()
+      startTimer()
     })
     .catch((error) => {
       console.log(error)
     })
 })
+
+onBeforeUnmount(() => {
+  clearInterval(timer)
+})
+
 </script>
 
 <template>
   <h1>Test af infoscreen</h1>
-  <InfoLine v-for="line in courses" :line="line"></InfoLine>
-  <!-- <div class="events">
-    <EventCard v-for="event in events" :key="event.id" :event="event" />
-  </div> -->
+        <div v-if="courses">
+          <div>
+            <InfoLine v-for="line in listData" :line="line"></InfoLine>
+          </div>
+          <p>{{ currentPage }} / {{ lastPage }}</p>
+        </div>
 </template>
 
 <style scoped>
-.events {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+
+.infinite-list-container {
+    height: 100%;
+    overflow: auto;
+    position: relative;
+    -webkit-overflow-scrolling: touch;
 }
 </style>
